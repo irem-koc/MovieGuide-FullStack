1c0df80d88154a053acad5e08e29ef9707dd4b29
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-11 23:33:33 +0300

    Add User entity with username, password, email, and reviews

---
+++ cinema-api/src/main/java/com/iremkoc/user_management_system/entities/User.java
@@ -0,0 +1,40 @@
+package com.iremkoc.user_management_system.entities;
+
+import java.util.List;
+
+import com.iremkoc.user_management_system.entities.concretes.Review;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
+}
