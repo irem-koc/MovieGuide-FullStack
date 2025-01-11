868e177b50b08096bc798831957e6a242dfc706d
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-11 23:33:22 +0300

    Add Movie entity with attributes and relationships

---
+++ cinema-api/src/main/java/com/iremkoc/user_management_system/entities/Movie.java
@@ -0,0 +1,43 @@
+package com.iremkoc.user_management_system.entities;
+
+import java.util.List;
+
+import com.iremkoc.user_management_system.entities.concretes.Review;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false)
+    private int releaseYear;
+
+    @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
+}
