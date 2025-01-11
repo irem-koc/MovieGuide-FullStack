28b99f5c9df36f840ea1e24ba663cfc127bcee21
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-11 23:33:37 +0300

    Add Review entity to user management system

---
+++ cinema-api/src/main/java/com/iremkoc/user_management_system/entities/concretes/Review.java
@@ -0,0 +1,41 @@
+package com.iremkoc.user_management_system.entities.concretes;
+
+import com.iremkoc.user_management_system.entities.Movie;
+import com.iremkoc.user_management_system.entities.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(length = 1000)
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
