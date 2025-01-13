00e771d50f5cfd920400c7e6978d6d70b51f9c83
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-13 23:37:02 +0300

    Add BusinessException class for handling business exceptions

---
+++ cinema-api/src/main/java/com/iremkoc/user_management_system/core/utilities/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.iremkoc.user_management_system.core.utilities.exceptions;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
