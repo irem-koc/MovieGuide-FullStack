cb282546d641680e8aa58755cdcb900dd6cf8158
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-14 23:26:37 +0300

    Add findByEmail method to UserRepository

--- cinema-api/src/main/java/com/iremkoc/user_management_system/dataAccess/abstracts/UserRepository.java
+++ cinema-api/src/main/java/com/iremkoc/user_management_system/dataAccess/abstracts/UserRepository.java
@@ -1,9 +1,13 @@
 package com.iremkoc.user_management_system.dataAccess.abstracts;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.iremkoc.user_management_system.entities.concretes.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
