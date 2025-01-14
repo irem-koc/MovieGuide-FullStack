2888b05560bd5a0a466d0a9705e3daf62d9ee9bc
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-14 23:41:01 +0300

    Add RegisterRequest to AuthenticateService

--- cinema-api/src/main/java/com/iremkoc/user_management_system/business/abstracts/AuthenticateService.java
+++ cinema-api/src/main/java/com/iremkoc/user_management_system/business/abstracts/AuthenticateService.java
@@ -1,11 +1,11 @@
 package com.iremkoc.user_management_system.business.abstracts;
 
 import com.iremkoc.user_management_system.business.requests.LoginRequest;
+import com.iremkoc.user_management_system.business.requests.RegisterRequest;
 import com.iremkoc.user_management_system.entities.concretes.Response;
-import com.iremkoc.user_management_system.entities.concretes.User;
 
 public interface AuthenticateService {
-    Response register(User user);
+    Response register(RegisterRequest registerRequest);
 
     Response login(LoginRequest loginRequest);
 }
