e8f2c5e865d003c373bb5373f8c31f8dbb657b8a
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-14 23:24:43 +0300

    Add AuthController for user authentication

---
+++ cinema-api/src/main/java/com/iremkoc/user_management_system/webApi/controllers/AuthController.java
@@ -0,0 +1,33 @@
+package com.iremkoc.user_management_system.webApi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iremkoc.user_management_system.business.concretes.AuthenticateManager;
+import com.iremkoc.user_management_system.business.requests.LoginRequest;
+import com.iremkoc.user_management_system.entities.concretes.Response;
+import com.iremkoc.user_management_system.entities.concretes.User;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticateManager authenticateManager;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = authenticateManager.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = authenticateManager.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+}
